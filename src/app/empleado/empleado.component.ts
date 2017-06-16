fe3c294b7dd2ba9e392f17dbea235b9bf550b19f
betuasaber10 <betuasaber10@gmail.com>
2017-06-15 19:25:26 -0500

    TypeScript
    Variables, Constructores, Clases y mas...

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import {  Empleado } from './empleado';
 
 @Component({
     selector: 'empleado',
     templateUrl: './empleado.component.html'
 })
 export class EmpleadoComponent{
-    titulo_componente_empleado = 'Componente de empleado';
+    public titulo = 'Componente Empleado: ';
+    public empleado:Empleado;
+    public trabajadores:Array<Empleado>;
+
+    constructor(){
+        //this.empleado = new Empleado('Humberto Mu;oz', 45, 'Cocinero', true);
+        this.trabajadores = [
+            new Empleado('Juan Perez', 5, 'Ingeniero', false),
+            new Empleado('Albert Einstein', 54, 'Fisico', true),
+            new Empleado('Pitagoras', 87, 'Matematico', true)
+        ];
+    }
+
+    ngOnInit(){        
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
 }
--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 export class FrutaComponent{
     public nombre_componente = 'Componente de fruta';
     public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
+    public nombre:string;
+    public edad:number;
+    public mayorDeEdad:boolean;
+    public trabajos:Array<any> = ['Carpintero', 22, 'Fontanero'];
+    comodin:any;
+    //para dar valor a las propiedades
+    constructor(){
+        this.nombre = 'Humberto Mu;oz';
+        this.edad = 77;
+        this.mayorDeEdad = false;
+        this.comodin = "SI";        
+    }
+    //primera funcion en cargarse despues del constructor
+    ngOnInit(){
+        this.cambiarNombre();
+        this.cambiarEdad(45);
+        console.log(this.nombre + " " + this.edad);
+
+        //variables y alcance
+        var uno = 8;
+        var dos = 15;
+
+        if(uno === 8){
+            let uno = 3;
+            var dos = 88;
+            console.log('dentro del if: ' + uno + " " + dos);
+        }
+        console.log('fuera del if: ' + uno + " " + dos);
+    }
+
+    cambiarNombre(){
+        this.nombre = 'Juan Perez';        
+    }
+    cambiarEdad(edad){
+        this.edad = edad;
+    }
 }
